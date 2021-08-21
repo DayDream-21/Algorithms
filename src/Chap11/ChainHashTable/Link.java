4ed367fa06ef9be7945115b33b6ae315a8293e42
DayDream-21 <vova13158@gmail.com>
2021-08-21 20:13:10 +0300

    Added Double and Chain Hash Table algorithm and HashFunc class with some hash creation functions

---
+++ src/Chap11/ChainHashTable/Link.java
@@ -0,0 +1,16 @@
+package Chap11.ChainHashTable;
+
+public class Link
+{ // (Здесь могут быть другие поля)
+    private int iData; // Данные
+    public Link next; // Следующий элемент списка
+    // -------------------------------------------------------------
+    public Link(int it) // Конструктор
+    { iData= it; }
+// -------------------------------------------------------------
+public int getKey()
+{ return iData; }
+    // -------------------------------------------------------------
+    public void displayLink() // Вывод содержимого элемента
+    { System.out.print(iData + " "); }
+}
